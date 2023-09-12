cce9e954c9ad720f4df4f871bc95257f84285060
dangchien2003 <chienboy03@gmail.com>
2023-09-13 01:36:29 +0700

    setup 3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+// thư viện code nodejs
 const express = require('express');
+// lấy ra phương thức 
 const app = express();
+app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.use(express.static('../public'));
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
 
 
 app.listen(3000, () => {
