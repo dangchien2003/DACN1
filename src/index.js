50542ace453dccd154d43c800b0d9153cf98a411
dangchien2003 <chienboy03@gmail.com>
2023-09-13 14:04:51 +0700

    setup views

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ const express = require('express');
 // lấy ra phương thức 
 const app = express();
 app.set('view engine', 'ejs');
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.use(express.static('../public'));
 
-
-
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.render("home");
 });
-
-
 
 app.listen(3000, () => {
     console.log(`127.0.0.1:3000`);
