c1429883ccd4a0b86010182cc61860bbe62d3333
dangchien2003 <chienboy03@gmail.com>
2023-10-02 21:14:03 +0700

    tree

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,10 +9,4 @@ function route(app) {
 }
 
 module.exports = route;
- sao k theo format t  lm sẵn có hiểu éo đâu đi. hỏi xong ngồi làm, hai đứa kia còn chưa hình dung được cái gì
- đây là ngôn ngữ mới sao độp phát ăn luôn được. lằng nhằng zl.app
- tối nay vào meet đi. nói thật là xem nát cái video kia ra rồi mà vẫn thấy lằng nhằng VarChar
- giờ chạy còn không biết
- giờ nhá mấy cái khó chưa nói định code giao diện trước mà có biết chạy để coi nó như nào đâu
- t bảo xem video tìm hiểu gg mà. thi
- 
+
