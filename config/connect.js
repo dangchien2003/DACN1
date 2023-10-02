da33821fa8cef6d8e04e76a20037d0d7cca34c8c
24-aianh <20211813@eaut.edu.vn>
2023-10-02 17:29:39 +0700

    up
    up

---
+++ config/connect.js
@@ -0,0 +1,17 @@
+/** @format */
+
+// /** @format */
+
+// const sql = require("mssql/msnodesqlv8");
+// var config = {
+//   database: "code",
+//   host: "(LocalDB)\MSSQLLocalDB",
+//   options: {
+//     trustedConnection: true,
+//   },
+// };
+
+// const conn = new sql.ConnectionPool(config).connect().then((pool) => {
+//   return pool;
+// });
+// module.exports = { sql, conn };
